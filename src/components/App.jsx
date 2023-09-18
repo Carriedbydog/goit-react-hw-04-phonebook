ed9992819fd6269d42d026463a2db80df54924c1
Vitalii Dimov <113688545+Carriedbydog@users.noreply.github.com>
2023-09-18 21:54:56 +0300

    refactoring with useState and useEffect

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Form } from './ContactForm/Form';
 import { ContactList } from './Contacts/ContactList';
@@ -6,20 +6,20 @@ import { Filter } from './Filter/Filter';
 import { nanoid } from 'nanoid';
 import { StyledSubTitle, StyledTitle, StyledWrapper } from 'styles/App.styled';
 
-export class App extends React.Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+export const App = () => {
+  const [contacts, setContacts] = useState(
+    JSON.parse(window.localStorage.getItem('contacts')) || []
+  );
+  const [filter, setFilter] = useState('');
 
-  handleAddContact = (name, number) => {
-    const { contacts } = this.state;
-    const item = contacts.find(item => item.name.toLowerCase() === name);
+  useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const handleAddContact = (name, number) => {
+    const item = contacts.find(
+      item => item.name.toLowerCase() === name.toLowerCase()
+    );
 
     if (item) {
       alert(`${name} is already in contacts`);
@@ -32,51 +32,42 @@ export class App extends React.Component {
       id: nanoid(),
     };
 
-    this.setState(prev => ({
-      contacts: [...prev.contacts, contact],
-    }));
+    setContacts(prev => [...prev, contact]);
   };
 
-  handleChangeFilter = e => {
-    this.setState({ filter: e.target.value });
+  const handleChangeFilter = e => {
+    setFilter(e.target.value);
   };
-  filterContacts = () => {
-    const normalizedFilter = this.state.filter.toLowerCase();
-    return this.state.contacts.filter(contact =>
+  const filterContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
       contact.name.toLowerCase().includes(normalizedFilter)
     );
   };
-  handleContactDelete = id => {
-    this.setState(prev => ({
-      contacts: prev.contacts.filter(contact => contact.id !== id),
-    }));
+  const handleContactDelete = id => {
+    setContacts(prev => prev.filter(contact => contact.id !== id));
   };
-  render() {
-    const { filter } = this.state;
-    const filteredData = this.filterContacts();
-    const deleteContact = this.handleContactDelete;
-    return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#010101',
-        }}
-      >
-        <StyledWrapper>
-          <StyledTitle>PhoneBook</StyledTitle>
-          <Form onAddContact={this.handleAddContact} />
-          <StyledSubTitle>Contacts</StyledSubTitle>
-          <Filter
-            inputFilterData={this.handleChangeFilter}
-            filterValue={filter}
-          />
-          <ContactList contacts={filteredData} onDelete={deleteContact} />
-        </StyledWrapper>
-      </div>
-    );
-  }
-}
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      <StyledWrapper>
+        <StyledTitle>PhoneBook</StyledTitle>
+        <Form onAddContact={handleAddContact} />
+        <StyledSubTitle>Contacts</StyledSubTitle>
+        <Filter inputFilterData={handleChangeFilter} filterValue={filter} />
+        <ContactList
+          contacts={filterContacts()}
+          onDelete={handleContactDelete}
+        />
+      </StyledWrapper>
+    </div>
+  );
+};
